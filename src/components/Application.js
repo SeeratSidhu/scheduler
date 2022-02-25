3a4d614055254d5ba243ab679f93ab646ea712ce
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-02-24 22:35:00 -0500

    combine states into an object

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,16 +46,19 @@ const appointments = [
 ];
 
 
-export default function Application(props) {
+export default function Application() {
 
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+    // appointments: {}
+  })
 
   const appointmentArray = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>);
   
   useEffect(() => {
     axios.get("/api/days")
-      .then(response => {
+      .then(response => { 
         setDays([...response.data]);
       })
   }, []);
@@ -71,8 +74,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
           />
         </nav>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,8 @@
 
-export function getAppointmentsForDay(state, selectedDay) {
+export function getAppointmentsForDay(state, day) {
   
-  const filteredDay = state.days.filter((day) => day.name === selectedDay);
-  
+  const filteredDay = state.days.filter((dayItem) => dayItem.name === day);
+
   if (filteredDay.length === 0) {
     return filteredDay;
   }
