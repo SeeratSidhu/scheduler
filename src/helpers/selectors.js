d7c423a41f957e38ed6ef1bc98a22ea6ccb53ddf
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-02-24 22:13:59 -0500

    fix test run issues

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,6 +3,9 @@ export function getAppointmentsForDay(state, selectedDay) {
   
   const filteredDay = state.days.filter((day) => day.name === selectedDay);
   
+  if (filteredDay.length === 0) {
+    return filteredDay;
+  }
 
   const appointmentsArray = filteredDay[0].appointments.map((appointment) => {
       return {...state.appointments[`${appointment}`]};
