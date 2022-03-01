64a4254c8696709eccd5096f115b164697fc633e
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-01 09:11:33 -0500

    update spots remaining on create and delete booking

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -9,16 +9,45 @@ const useApplicationData = () => {
     interviewers: {}
   });
 
+  function findRemainingSpots (id, state) {
+    const selectedDay = state.days.findIndex(day => day.appointments.includes(id));
+
+    const emptyInterviews = state.days[selectedDay].appointments.filter(appointment => !state.appointments[`${appointment}`].interview)
+    const spots = emptyInterviews.length;
+
+    return {spots, selectedDay}
+  }
+
   function bookInterview(id, interview) {
     //overwrite the existing interview object with the interview from argument
     const appointment = {...state.appointments[id], interview: { ...interview}};
     //update appointments object with the new appointment
     const appointments = {...state.appointments, [id]: appointment}
-
     
+
     return axios.put(`/api/appointments/${id}`, {interview})
     .then((response) => {
-      return setState({...state, appointments});
+      return setState((prev) => ({...prev, appointments}));
+    })
+    .then(data => {
+      
+      // const day = {...state.days[selectedDay], spots};
+      // const daysCopy = [...state.days];
+      // daysCopy.splice(selectedDay, 1, day);
+      
+      return setState((prev) => {
+        const {spots, selectedDay} = findRemainingSpots(id, prev);
+        console.log(spots)
+        return {...prev, 
+          days: [
+            ...prev.days.slice(0, selectedDay),
+            {
+              ...prev.days[selectedDay],
+              spots: spots
+            },
+            ...prev.days.slice(selectedDay + 1)
+          ]
+        }});
     })
 
   }
@@ -30,6 +59,26 @@ const useApplicationData = () => {
     return axios.delete(`/api/appointments/${id}`)
       .then((response) => {
         return setState({...state, appointments});
+      })
+      .then(data => {
+      
+        // const day = {...state.days[selectedDay], spots};
+        // const daysCopy = [...state.days];
+        // daysCopy.splice(selectedDay, 1, day);
+        
+        return setState((prev) => {
+          const {spots, selectedDay} = findRemainingSpots(id, prev);
+          console.log(spots)
+          return {...prev, 
+            days: [
+              ...prev.days.slice(0, selectedDay),
+              {
+                ...prev.days[selectedDay],
+                spots: spots
+              },
+              ...prev.days.slice(selectedDay + 1)
+            ]
+          }});
       })
   }
 
