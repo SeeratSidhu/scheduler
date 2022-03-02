726c015b7438af083d4aca4f2f5fc20f1badf73a
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-02 16:18:47 -0500

    test the editing functionality using cypress

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -26,4 +26,22 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+
+    cy.get(".appointment__card--show")
+      .find("[alt=Edit]")
+      .click({force: true});
+
+    cy.get("[data-testid=student-name-input]").clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
